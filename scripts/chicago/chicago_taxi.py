cf787e0bab89f50da6c0c84d9be374a7fae469ee
Becky Mathew <codebecky@gmail.com>
2020-03-08 12:15:49 -0400

    more chicago taxi data

--- scripts/chicago/chicago_taxi.py
+++ scripts/chicago/chicago_taxi.py
@@ -17,9 +17,10 @@ client = Socrata("data.cityofchicago.org",
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("wrvz-psew", limit=2000)
+# results = client.get("wrvz-psew", limit=2000)
+results = client.get("wrvz-psew", where="trip_start_timestamp between '2019-01-01T00:00:00' and '2019-01-31T23:59:59'")
 
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
-results_df.to_csv("C:\\Users\\CodeB\\Documents\\GitHub\\transportation-transformation\\data\\chicago\\chicago_taxi_2000_rows.csv")
+results_df.to_csv("C:\\Users\\CodeB\\Documents\\GitHub\\transportation-transformation\\data\\chicago\\chicago_taxi_2019_01_v1.csv")
 print(results_df.describe)
--- scripts/chicago/clean_chicago_taxi.py
+++ scripts/chicago/clean_chicago_taxi.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import json
 
-data = pd.read_csv("C:\\Users\\CodeB\\Documents\\GitHub\\transportation-transformation\\data\\chicago\\chicago_taxi_2000_rows.csv")
+data = pd.read_csv("C:\\Users\\CodeB\\Documents\\GitHub\\transportation-transformation\\data\\chicago\\chicago_taxi_2019_01.csv")
 df = pd.DataFrame(data)
 # usecols=["trip_start_timestamp", "trip_end_timestamp", "trip_seconds", "trip_miles", "fare", "tips", "tolls", "extras", "trip_total",  "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude"]
 
 # Drop NAs for pickup / dropoff lat-lon. Drops 203 rows (from 2000 rows to 1797 rows)
 df_no_na = df.dropna(subset=["pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude"])
+print(df_no_na.shape)
 
 # Split timestamp into date and time columns
 df = df.assign(trip_start_date=df['trip_start_timestamp'].str[:10])
@@ -22,9 +23,12 @@ date_df = df.groupby(["trip_start_date"]).count()
 desc_stats = date_df["trip_end_date"].describe()
 print("daily ridership descriptive stats: ", desc_stats)
 
-desc_stats_dict = desc_stats.to_dict()
-with open("chi_taxi_daily_ridership_descriptive_stats.json", "w") as fout:
-    json.dump(desc_stats_dict, fout)
+duplicated = df.duplicated()
+print(duplicated.value_counts())
+
+# desc_stats_dict = desc_stats.to_dict()
+# with open("chi_taxi_2019_01_daily_ridership.json", "w") as fout:
+#     json.dump(desc_stats_dict, fout)
 
 # TODO: output format?
 # TODO: Drop census tracts, community areas, tips, tolls, extras, payment_type, locations, computed region, company
